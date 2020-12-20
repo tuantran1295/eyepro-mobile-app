f3a6800ca65fdfc6f70f68292887fe54c1a88c41
Hồ Xuân Hùng <38959941+xuanhungho@users.noreply.github.com>
2020-12-20 12:23:45 +0700

    add ws

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,14 +1,63 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {LoginService} from '../../services/login.service';
-import {AlertController, LoadingController} from '@ionic/angular';
-import {Router} from '@angular/router';
-import {HttpHeaders} from '@angular/common/http';
-import {Plugins} from '@capacitor/core';
-import {ClassRoomService} from '../../services/class-room.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import { Plugins } from '@capacitor/core';
+import { ClassRoomService } from '../../services/class-room.service';
 
-const {Storage} = Plugins;
 
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+export class WebSocketAPI {
+    webSocketEndPoint: string = 'http://27.71.228.53:9002/SmartClass/student-websocket';
+    topic: string = "/topic/newMonitor/B6";
+    stompClient: any;
+
+    _connect() {
+        console.log("Initialize WebSocket Connection");
+        let ws = new SockJS(this.webSocketEndPoint);
+        this.stompClient = Stomp.over(ws);
+        const _this = this;
+        _this.stompClient.connect({}, function (frame) {
+            _this.stompClient.subscribe(_this.topic, function (sdkEvent) {
+                _this.onMessageReceived(sdkEvent);
+            });
+            //_this.stompClient.reconnect_delay = 2000;
+        }, this.errorCallBack);
+    };
+
+    _disconnect() {
+        if (this.stompClient !== null) {
+            this.stompClient.disconnect();
+        }
+        console.log("Disconnected");
+    }
+
+    // on error, schedule a reconnection attempt
+    errorCallBack(error) {
+        console.log("errorCallBack -> " + error)
+        setTimeout(() => {
+            this._connect();
+        }, 5000);
+    }
+
+    /**
+     * Send message to sever via web socket
+     * @param {*} message 
+     */
+    _send(message) {
+        console.log("calling logout api via web socket");
+        this.stompClient.send("/app/hello", {}, JSON.stringify(message));
+    }
+
+    onMessageReceived(message) {
+        console.log("Message Recieved from Server :: " + message);
+    }
+}
+
+const { Storage } = Plugins;
 
 @Component({
     selector: 'app-login',
@@ -17,6 +66,26 @@ const {Storage} = Plugins;
 })
 export class LoginPage implements OnInit {
     credentials: FormGroup;
+
+    webSocketAPI: WebSocketAPI;
+    greeting: any;
+    name: string;
+
+    connect() {
+        this.webSocketAPI._connect();
+    }
+
+    disconnect() {
+        this.webSocketAPI._disconnect();
+    }
+
+    sendMessage() {
+        this.webSocketAPI._send(this.name);
+    }
+
+    handleMessage(message) {
+        this.greeting = message;
+    }
 
     constructor(
         private formBuilder: FormBuilder,
@@ -29,6 +98,8 @@ export class LoginPage implements OnInit {
     }
 
     ngOnInit() {
+        this.webSocketAPI = new WebSocketAPI();
+        this.connect();
         this.autoLogin();
         this.credentials = this.formBuilder.group({
             userName: ['', [Validators.required, Validators.minLength(3)]],
@@ -81,14 +152,13 @@ export class LoginPage implements OnInit {
                 if (chosenClass) {
                     console.log("Login Page CHOSEN CLASS:");
                     console.log(chosenClass);
-                    this.router.navigateByUrl('/thong-tin-lop/' + chosenClass, {replaceUrl: true});
+                    this.router.navigateByUrl('/thong-tin-lop/' + chosenClass, { replaceUrl: true });
                 } else {
-                    this.router.navigateByUrl('/danh-sach-lop', {replaceUrl: true});
+                    this.router.navigateByUrl('/danh-sach-lop', { replaceUrl: true });
                 }
             });
         });
 
     }
-
 
 }
