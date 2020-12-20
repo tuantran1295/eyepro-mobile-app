ebbf0fc4bf67fe241694b650a2fb33157ed028b2
Hồ Xuân Hùng <38959941+xuanhungho@users.noreply.github.com>
2020-12-20 12:28:32 +0700

    thêm websocket vào quan-li-lop

---
+++ README.md
@@ -0,0 +1,19 @@
+# App mobile của cụ Tuấn
+<!-- ![VDSMART](http://27.71.228.53:9002/SmartClass/assets/img/smartclass-mini.png) -->
+
+## Installation
+
+1. Install Node v10.0.5
+
+2. Install dependency:
+
+``` bash
+$ npm install -g @angular/cli
+$ npm install
+```
+
+## Run project
+
+``` bash
+$ ng serve --host 0.0.0.0
+```
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,58 +7,7 @@ import { HttpHeaders } from '@angular/common/http';
 import { Plugins } from '@capacitor/core';
 import { ClassRoomService } from '../../services/class-room.service';
 
-
-import * as Stomp from 'stompjs';
-import * as SockJS from 'sockjs-client';
-export class WebSocketAPI {
-    webSocketEndPoint: string = 'http://27.71.228.53:9002/SmartClass/student-websocket';
-    topic: string = "/topic/newMonitor/B6";
-    stompClient: any;
-
-    _connect() {
-        console.log("Initialize WebSocket Connection");
-        let ws = new SockJS(this.webSocketEndPoint);
-        this.stompClient = Stomp.over(ws);
-        const _this = this;
-        _this.stompClient.connect({}, function (frame) {
-            _this.stompClient.subscribe(_this.topic, function (sdkEvent) {
-                _this.onMessageReceived(sdkEvent);
-            });
-            //_this.stompClient.reconnect_delay = 2000;
-        }, this.errorCallBack);
-    };
-
-    _disconnect() {
-        if (this.stompClient !== null) {
-            this.stompClient.disconnect();
-        }
-        console.log("Disconnected");
-    }
-
-    // on error, schedule a reconnection attempt
-    errorCallBack(error) {
-        console.log("errorCallBack -> " + error)
-        setTimeout(() => {
-            this._connect();
-        }, 5000);
-    }
-
-    /**
-     * Send message to sever via web socket
-     * @param {*} message 
-     */
-    _send(message) {
-        console.log("calling logout api via web socket");
-        this.stompClient.send("/app/hello", {}, JSON.stringify(message));
-    }
-
-    onMessageReceived(message) {
-        console.log("Message Recieved from Server :: " + message);
-    }
-}
-
 const { Storage } = Plugins;
-
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
@@ -66,26 +15,6 @@ const { Storage } = Plugins;
 })
 export class LoginPage implements OnInit {
     credentials: FormGroup;
-
-    webSocketAPI: WebSocketAPI;
-    greeting: any;
-    name: string;
-
-    connect() {
-        this.webSocketAPI._connect();
-    }
-
-    disconnect() {
-        this.webSocketAPI._disconnect();
-    }
-
-    sendMessage() {
-        this.webSocketAPI._send(this.name);
-    }
-
-    handleMessage(message) {
-        this.greeting = message;
-    }
 
     constructor(
         private formBuilder: FormBuilder,
@@ -98,8 +27,6 @@ export class LoginPage implements OnInit {
     }
 
     ngOnInit() {
-        this.webSocketAPI = new WebSocketAPI();
-        this.connect();
         this.autoLogin();
         this.credentials = this.formBuilder.group({
             userName: ['view_301', [Validators.required, Validators.minLength(3)]],
